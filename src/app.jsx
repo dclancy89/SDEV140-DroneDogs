89bd1b2c5279fb9b6469aa877013e18010b88aed
Daniel Clancy <daniel@danieltclancy.com>
2021-03-08 19:31:05 -0600

    added logic to calculate the order info.

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -32,14 +33,43 @@ const useStyles = makeStyles((theme) => ({
     flexAround: {
         display: 'flex',
         justifyContent: 'space-around',
+    },
+
+    marginRight: {
+        marginRight: '15px',
     }
 
 }));
 
+const DBL_SALES_TAX_RATE = 0.07;
+const DBL_DOG_PRICE = 1.99;
+
 
 export default function App() {
+    const [numBeefDogs, setNumBeefDogs] = useState(0);
+    const [numPorkDogs, setNumPorkDogs] = useState(0);
+    const [numTurkeyDogs, setNumTurkeyDogs] = useState(0);
+    const [subTotal, setSubTotal] = useState(0);
+    const [salesTax, setSalesTax] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
     
     const classes = useStyles();
+
+    const calculateTotal = useCallback(() => {
+        let intNumTotalDogs = parseInt(numBeefDogs) + parseInt(numPorkDogs) + parseInt(numTurkeyDogs);
+        let dblSubTotal = intNumTotalDogs * DBL_DOG_PRICE;
+        setSubTotal(dblSubTotal);
+
+        let dblSalesTax = dblSubTotal * DBL_SALES_TAX_RATE;
+        setSalesTax(dblSalesTax.toFixed(2));
+
+        let dblTotalCost = dblSubTotal + dblSalesTax;
+        setTotalCost(dblTotalCost.toFixed(2));
+    }, [numBeefDogs, numPorkDogs, numTurkeyDogs]);
+
+    const closeApp = useCallback(() => {
+        app.exit(0);
+    });
 
     return (
         <div>
@@ -52,27 +82,27 @@ export default function App() {
                     <img src={logo} className={classes.logo} alt="Drone Dog Alternative" />
                 </Grid>
                 <Grid item xs={8}>
-                    <TextField id="numBeefDogs" label="# Beef Dogs" variant="outlined" />
+                    <TextField id="numBeefDogs" label="# Beef Dogs" variant="outlined" onChange={e => setNumBeefDogs(e.target.value)}/>
                 </Grid>
                 <Grid item xs={8}>
-                    <TextField id="numPorkDogs" label="# Pork Dogs" variant="outlined" />
+                    <TextField id="numPorkDogs" label="# Pork Dogs" variant="outlined" onChange={e => setNumPorkDogs(e.target.value)} />
                 </Grid>
                 <Grid item xs={8}>
-                    <TextField id="numTurkeyDogs" label="# Turkey Dogs" variant="outlined" />
+                    <TextField id="numTurkeyDogs" label="# Turkey Dogs" variant="outlined" onChange={e => setNumTurkeyDogs(e.target.value)} />
                 </Grid>
                 <Grid item xs={12}>
-                    <Button variant="contained" color="primary">Calculate Order</Button>
-                    <Button variant="contained" color="primary">Submit Order</Button>
-                    <Button variant="contained" color="primary">Exit</Button>
+                    <Button className={classes.marginRight} variant="contained" color="primary" onClick={calculateTotal}>Calculate Order</Button>
+                    <Button className={classes.marginRight} variant="contained" color="primary">Submit Order</Button>
+                    <Button className={classes.marginRight} variant="contained" color="primary" onClick={closeApp}>Exit</Button>
                 </Grid>
                 <Grid item xs={8}>
-                    <TextField id="subtotal" label="Subtotal" variant="outlined" disabled />
+                    <TextField id="subtotal" label="Subtotal" variant="outlined" value={"$" + subTotal} onChange={e => setSubTotal(e.target.value)} />
                 </Grid>
                 <Grid item xs={8}>
-                    <TextField id="tax" label="Sales Tax" variant="outlined" disabled />
+                    <TextField id="tax" label="Sales Tax" variant="outlined" value={"$" + salesTax} onChange={e => setSalesTax(e.target.value)} />
                 </Grid>
                 <Grid item xs={8}>
-                    <TextField id="totalCost" label="Total Cost" variant="outlined" disabled />
+                    <TextField id="totalCost" label="Total Cost" variant="outlined" value={"$" + totalCost} onChange={e => setTotalCost(e.target.value)} />
                 </Grid>
             </Grid>
         </div>
